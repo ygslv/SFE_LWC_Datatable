34051e23345fb7d0e4c80f9531c5c605b5ac516e
ygslv <why.did.i.become.ashen@gmail.com>
2022-05-03 16:00:24 +0300

    Create filtered fields markup and add filter change handler

--- src/lwc/sfe_ReportFilter/sfe_ReportFilter.js
+++ src/lwc/sfe_ReportFilter/sfe_ReportFilter.js
@@ -1,5 +1,18 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
 
 export default class SfeReportFilter extends LightningElement {
 
+    @api reportFilterData;
+
+    handleFilterChange(event) {
+        Object.keys(this.reportFilterData).forEach(prop => {
+
+            const fieldName = event.target.name ? event.target.name : event.target.fieldName;
+            const targetElement = this.reportFilterData[prop].find(elem => elem.fieldName === fieldName);
+            if (targetElement) {
+                targetElement.value = event.detail.value;
+                return;
+            }
+        });
+    }
 }
